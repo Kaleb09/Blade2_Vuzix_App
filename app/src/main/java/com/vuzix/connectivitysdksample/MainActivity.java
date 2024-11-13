1256b02dd33739c24bc29bb438fadd295f4f1fb5
kaleb <blekaadamu979@gmail.com>
2024-11-14 00:08:53 +0300

    image send functionality aded

--- app/src/main/java/com/vuzix/connectivitysdksample/MainActivity.java
+++ app/src/main/java/com/vuzix/connectivitysdksample/MainActivity.java
@@ -5,13 +5,21 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.vuzix.connectivity.sdk.Connectivity;
 import com.vuzix.connectivity.sdk.Device;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends Activity {
 
@@ -21,7 +29,8 @@ public class MainActivity extends Activity {
     private static final String EXTRA_TEXT = "text";
 
     private EditText mEditText;
-
+     ImageView imageView;
+     Button sendimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +44,25 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.main);
         mEditText = findViewById(R.id.text);
+        sendimage = findViewById(R.id.sendimage);
+        imageView = findViewById(R.id.imageView);
+        sendimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Drawable drawable = getResources().getDrawable(R.drawable.samplepic);
+                Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+                Intent sendIntent = new Intent(ACTION_SEND);
+                sendIntent.setPackage("com.vuzix.connectivitysdksample");
+
+                sendIntent.putExtra(EXTRA_TEXT, byteArray);
+                Connectivity.get(MainActivity.this).sendBroadcast(sendIntent);
+                mEditText.setText(null);
+            }
+        });
     }
 
     @Override
@@ -80,9 +108,16 @@ public class MainActivity extends Activity {
         public void onReceive(Context context, Intent intent) {
             if (Connectivity.get(context).verify(intent, "com.vuzix.connectivitysdksample")) {
                 String text = intent.getStringExtra(EXTRA_TEXT);
+
                 if (text != null) {
                     Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
                 }
+                byte[] byteArray =intent.getByteArrayExtra("message");
+                  if(byteArray!=null) {
+                      Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+                      imageView.setImageBitmap(bitmap);
+                  }
+
             }
         }
     };
